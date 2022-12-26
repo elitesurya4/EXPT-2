c9f989cb64d933775c330f1f3a39da6d4d788691
Suryanshu Patnaik <er.suryanshupatnaik.4@gmail.com>
2022-12-26 11:57:13 +0530

    main

---
+++ fullwave/script.js
@@ -0,0 +1,6 @@
+function alert_1(){
+    alert("Full Wave Rectification on Full Wave Bridge Rectifier")
+}
+function alert_2(){
+    alert("Full Wave Rectification on Center Tapped Full Wave Rectifier")
+}
